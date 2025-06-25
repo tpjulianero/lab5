58162f71e8700769b7b160249a63179fe6b0a195
Julio Ochoa <jochoam8@est.ups.edu.ec>
2025-06-25 18:56:58 -0500

    Manejo de Excepciones

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from librodiario import LibroDiario
+from librodiario import LibroDiario, MontoInvalidoError
 
 libro = LibroDiario()
+try:
+    libro.agregar_transaccion('18/06/2025', 'Compra de laptop', 780, 'egreso')
 
-libro.agregar_transaccion('18/06/2025', 'Compra de laptop', 780, 'egreso')
+    libro.agregar_transaccion('18/06/2025', 'Venta de sensor TK-110', 780, 'ingreso')
 
-#libro.agregar_transaccion('18/06/2025', 'Venta de sensor TK-110', 780, 'Ingreso')
+    libro.agregar_transaccion('18/06/2025', 'Compra de software', 250, 'egreso')
 
-libro.agregar_transaccion('18/06/2025', 'Compra de insumos de oficina', -85.60, 'egreso')
-
+    libro.agregar_transaccion('18/06/2025', 'Compra de insumos de oficina', 85.60, 'egreso')
+except (ValueError, MontoInvalidoError) as e:
+    print (f"Error: {e}")
 print(libro.calcular_resumen())
--- librodiario.py
+++ librodiario.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 from typing import List, Dict
 
-class LibroDiario:
+class MontoInvalidoError(Exception):
+    pass
+
+class LibroDiario:    
     """Gestión contable básica de ingresos y egresos."""
 
     def __init__(self):
@@ -9,11 +12,22 @@ class LibroDiario:
 
     def agregar_transaccion(self, fecha: str, descripcion: str, monto: float, tipo: str) -> None:
         """Agrega una transacción al libro diario."""
-        if tipo not in ("ingreso", "egreso"):
-            raise ValueError("Tipo de transacción inválido. Use 'ingreso' o 'egreso'.")
+        
+        tipo = tipo.lower()
 
+        if tipo not in ("ingreso", "egreso"):
+            raise ValueError(f"Tipo de transacción inválido({tipo}). Use 'ingreso' o 'egreso'.")
+        
+        try:
+            obj_fecha= datetime.strptime(fecha, "%d/%m/%Y")    
+        except Exception as e:
+            raise ValueError(f"Formato de fecha inválido({fecha}). Use 'dd/mm//yyyy'.")
+        
+        if monto < 0:
+            raise MontoInvalidoError(f"Monto inválido ({monto}). El monto debe ser mayor a 0")
+        
         transaccion = {
-            "fecha": datetime.strptime(fecha, "%d/%m/%Y"),
+            "fecha": obj_fecha,
             "descripcion": descripcion,
             "monto": monto,
             "tipo": tipo
@@ -22,6 +36,7 @@ class LibroDiario:
 
     def calcular_resumen(self) -> Dict[str, float]:
         """Devuelve el resumen total de ingresos y egresos."""
+
         resumen = {"ingresos": 0.0, "egresos": 0.0}
         for transaccion in self.transacciones:
             if transaccion["tipo"] == "ingreso":
